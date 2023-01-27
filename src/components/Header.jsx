51de9743948a7e0c11b1ee09e75e4dad7c51422e
Skrypnykov <oskripnikov@gmail.com>
2023-01-27 22:11:34 +0200

    #4: React Pizza

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg';
 
 import Button from './Button';
@@ -9,6 +10,7 @@ function Header() {
   return (
     <div className="header">
       <div className="container">
+        <Link to='/'>
         <div className="header__logo">
           <img width="38" src={logoSvg} alt="Pizza logo" />
           <div>
@@ -16,7 +18,9 @@ function Header() {
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </div>
+        </Link>
         <div className="header__cart">
+        <Link to='/cart'>
           <Button className="button--cart">
             <span>278 ₴</span>
             <div className="button__delimiter"></div>
@@ -51,6 +55,7 @@ function Header() {
             </svg>
             <span>2</span>
           </Button>
+          </Link>
         </div>
       </div>
     </div>
